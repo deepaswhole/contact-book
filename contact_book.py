7a2923d1588c71305a53ea617f39810edc0249a4
Deepaswhole <lanwong0419@gmail.com>
2024-12-30 16:31:20 +0800

    Add functionality to add new contacts

--- contact_book.py
+++ contact_book.py
@@ -1,6 +1,7 @@
 print("Welcome to the Contact Book!")
 contacts = []
 
+
 def show_contacts():
     if not contacts:
         print("No contacts found!")
@@ -9,4 +10,7 @@ def show_contacts():
             print(f"{i}. Name: {contact['name']}, Phone: {contact['phone']}, Email: {contact['email']}")
 
 
+def add_contact(name, phone, email):
+    contacts.append({"name": name, "phone": phone, "email": email})
+    print(f"Contact {name} added successfully!")
 
